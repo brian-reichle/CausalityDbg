886894bd3d3d0c03c2ab9229682a0c4f1d0ed025
Brian Reichle <bwreichle+git@gmail.com>
2018-12-27 13:58:20 +1100

    Fix CorDebugIntercept namespace.

--- src/CausalityDbg.Core/Native/CorDebugApi/Enums/CorDebugIntercept.cs
+++ src/CausalityDbg.Core/Native/CorDebugApi/Enums/CorDebugIntercept.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Brian Reichle.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 using System;
 
-namespace CausalityDbg.Core
+namespace CausalityDbg.Core.CorDebugApi
 {
 	[Flags]
 	enum CorDebugIntercept
