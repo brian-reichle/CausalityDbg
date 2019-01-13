e6811131a6438dacc7acfde7bf2a3e5a49fb43d9
Brian Reichle <bwreichle+git@gmail.com>
2019-01-14 08:11:34 +1100

    Removed unused usings.

--- src/CausalityDbg.Main/Extensions/EnvironmentProperties.cs
+++ src/CausalityDbg.Main/Extensions/EnvironmentProperties.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Brian Reichle.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 using System.Windows;
 using CausalityDbg.Configuration;
-using CausalityDbg.Core;
 using CausalityDbg.Source;
 
 namespace CausalityDbg.Main
