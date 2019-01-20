6b2341a8c88931c2c004b6aae41a85d1cf6a82ba
Brian Reichle <bwreichle+git@gmail.com>
2019-01-20 19:04:35 +1100

    Avoid some locks with ConcurrentDictionary.

--- src/CausalityDbg.Main/Controls/TimelineColors.cs
+++ src/CausalityDbg.Main/Controls/TimelineColors.cs
@@ -1,5 +1,5 @@
 // Copyright (c) Brian Reichle.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Windows;
 using System.Windows.Media;
 
@@ -14,39 +14,23 @@ namespace CausalityDbg.Main
 
 		public static Brush GetBrush(int color)
 		{
-			lock (_brushes)
+			return _brushes.GetOrAdd(color, c =>
 			{
-				if (!_brushes.TryGetValue(color, out var brush))
-				{
-					var blue = (byte)(color & 0xFF);
-					var green = (byte)((color >> 8) & 0xFF);
-					var red = (byte)((color >> 16) & 0xFF);
+				var blue = (byte)(color & 0xFF);
+				var green = (byte)((color >> 8) & 0xFF);
+				var red = (byte)((color >> 16) & 0xFF);
 
-					brush = new SolidColorBrush(Color.FromRgb(red, green, blue));
-					brush.Freeze();
-					_brushes.Add(color, brush);
-				}
-
+				var brush = new SolidColorBrush(Color.FromRgb(red, green, blue));
+				brush.Freeze();
 				return brush;
-			}
+			});
 		}
 
 		public static Pen GetPen(int color)
-		{
-			lock (_pens)
-			{
-				if (!_pens.TryGetValue(color, out var pen))
-				{
-					pen = new Pen(GetBrush(color), 1);
-					_pens.Add(color, pen);
-				}
+			=> _pens.GetOrAdd(color, c => new Pen(GetBrush(c), 1));
 
-				return pen;
-			}
-		}
-
-		static readonly Dictionary<int, Brush> _brushes = new Dictionary<int, Brush>();
-		static readonly Dictionary<int, Pen> _pens = new Dictionary<int, Pen>();
+		static readonly ConcurrentDictionary<int, Brush> _brushes = new ConcurrentDictionary<int, Brush>();
+		static readonly ConcurrentDictionary<int, Pen> _pens = new ConcurrentDictionary<int, Pen>();
 
 		static T Freeze<T>(T value)
 			where T : Freezable
